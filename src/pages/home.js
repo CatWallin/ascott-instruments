f43163c7e13efc7cb48cac76b87e487d8926337c
Cat Wallin <catherine.wallin@gmail.com>
2022-06-11 13:25:38 -0700

    working on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import BannerImage from '../images/DSC_4837.jpg';
+import "../styles/Home.css";
 
 const Home = () => {
     return (
-      <div class="home">
-        <h1>
-          Home
-        </h1>
+      <div className="home">
+        <div className="headerContainer" style={{ backgroundImage: `url(${BannerImage})` }}>
+          <h1> A. Scott </h1>
+          <h1> Instruments </h1>
+          <p> Quality Crafted </p>
+          </div>
       </div>
     );
   };
