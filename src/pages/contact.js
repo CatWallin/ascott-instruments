6fc47206ff578f5c5cdee6121a8561f658bcff81
Cat Wallin <catherine.wallin@gmail.com>
2022-06-14 17:13:44 -0700

    contact in progress

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Image from '../images/DSC_4840.jpg';
 import '.././App.css';
+import "../styles/Contact.css";
+import { BsInstagram } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
+import { CgWebsite } from "react-icons/cg";
   
 const Contact = () => {
   return (
@@ -9,7 +13,50 @@ const Contact = () => {
         <img src={Image} alt="contactPic" height={700} width={500}></img>
       </div>
       <div className="contactRight">
-
+        <html>
+          <div>
+            <h1>Let's get in touch!</h1>
+            <div className="socialMedia">
+              <div>
+                <a href="http://www.instagram.com/ascottinstruments/">
+                  <button>
+                    <BsInstagram />
+                    <text> @ascottinstruments </text>
+                  </button>
+                </a>
+              </div>
+              <div>
+                <a href="giotariseal0812@gmail.com">
+                  <button>
+                    <MdOutlineEmail />
+                    <text> giotariseal0812@gmail.com </text>
+                  </button>
+                </a>
+              </div>
+              <div></div>
+                <a href="https://reverb.com/shop/aarons-gear-garage-908#">
+                  <button>
+                    <CgWebsite />
+                    <text> Reverb Shop </text>
+                  </button>
+                </a>
+              </div>
+          </div>
+          <head>
+            <title>Contact Me</title>
+          </head>
+          <body>
+            <form id="contactForm" className="form">
+              <label>Name</label>
+              <input type ="text" name="name"></input>
+              <label>Email</label>
+              <input type ="email" name="email"></input>
+              <label>Message</label>
+              <textarea type="message"></textarea>
+              <input type="submit" value="Send"></input>
+            </form>
+          </body>
+        </html>
       </div>
     </div>
   );
