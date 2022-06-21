7c702878a74e13d6795fdb48574bf9da8029853a
Cat Wallin <catherine.wallin@gmail.com>
2022-06-21 15:34:32 -0700

    prices description updated to display properly

--- src/PriceItem.js
+++ src/PriceItem.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 function PriceItem({name, price, description}) {
     return (
+        
         <div className="PriceItem">
             <h1> {name} </h1>
-            <p> ${price} </p>
-            <p> {description} </p>
+            <h2> ${price} </h2>
+            <text className="description">
+                {description.map((message) => <p> {message} </p>)}
+            </text>
         </div>
     )
 }
--- src/PriceList.js
+++ src/PriceList.js
@@ -20,30 +20,30 @@ export const PriceList = [
         name: "Irish Bouzouki",
         price: 1000.00,
         description: ["Scale Length: 24 inches", 
-        "Back/Sides: Hunduran Mahogany",
-        "Top: Sitka Spruce",
-        "Binding: Rosewood",
-        "Rossette: Rosewood",
-        "Neck: Hunduran Mahogany",
-        "Fretboard: Ebony",
-        "Headstock Plate: African Blackwood",
-        "Tuning Machines: Gotoh F Style",
-        "Tail Piece: Scallop",
-        "Bridge: Ebony"
-        ]
+                    "Back/Sides: Hunduran Mahogany",
+                    "Top: Sitka Spruce",
+                    "Binding: Rosewood",
+                    "Rossette: Rosewood",
+                    "Neck: Hunduran Mahogany",
+                    "Fretboard: Ebony",
+                    "Headstock Plate: African Blackwood",
+                    "Tuning Machines: Gotoh F Style",
+                    "Tail Piece: Scallop",
+                    "Bridge: Ebony"
+                    ]
     },
     {
         name: "Cittern",
         price: 1200.00,
         description: ["Scale Length: 22 inches", 
-        "Back/Sides: Hunduran Mahogany",
-        "Top: Sitka Spruce",
-        "Binding: Rosewood",
-        "Rossette: Rosewood",
-        "Neck: Hunduran Mahogany",
-        "Fretboard: Ebony",
-        "Headstock Plate: African Blackwood",
-        "Bridge: Ebony"
-        ]
+                    "Back/Sides: Hunduran Mahogany",
+                    "Top: Sitka Spruce",
+                    "Binding: Rosewood",
+                    "Rossette: Rosewood",
+                    "Neck: Hunduran Mahogany",
+                    "Fretboard: Ebony",
+                    "Headstock Plate: African Blackwood",
+                    "Bridge: Ebony"
+                    ]
     }
 ]
