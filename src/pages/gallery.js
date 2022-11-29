6eec97827c80ad6031ebb2e33868cf7ed0864031
Cat Wallin <catherine.wallin@gmail.com>
2022-11-29 14:50:07 -0800

    formatted slideshow

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -7,9 +7,6 @@ const Gallery = () => {
   return (
     
     <div class="galleryPage">
-      <h1>
-        Gallery
-      </h1>
       {/* <div className="gallery"> 
         {GalleryList.map((galleryItem, key) => {
           return (
@@ -21,8 +18,9 @@ const Gallery = () => {
         })}
       </div>
       */} 
-      <div>
-      <iframe src="https://catwallinphotography.smugmug.com/frame/slideshow?key=zwTtPD&speed=3&transition=fade&autoStart=1&captions=0&navigation=1&playButton=1&randomize=1&transitionSpeed=2" width="800" height="600" frameborder="no" scrolling="no" title="slideshow"></iframe>      </div>
+      <div className="gallery">
+      <iframe src="https://catwallinphotography.smugmug.com/frame/slideshow?key=zwTtPD&speed=3&transition=fade&autoStart=1&captions=0&navigation=1&playButton=1&randomize=1&transitionSpeed=2" width="800" height="600" frameborder="no" scrolling="no" title="slideshow"></iframe>      
+      </div>
     </div>
   );
 };
