11dc9a9764417f7f6c41faca00de408a0377b33d
Cat Wallin <catherine.wallin@gmail.com>
2022-12-17 14:13:10 -0800

    working on links

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <Navbar />
-        <Routes>
+      <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
           <Route path='/contact' element={<Contact/>} />
